46df984d9def832c23283af7a6d72540a17c9506
Rajput.NJ <100520546+Rajput-Neha-Jitendra@users.noreply.github.com>
2025-07-01 17:09:51 +0530

    Create NumberPK.java

---
+++ Java_Projects/Number Checker/NumberPK.java
@@ -0,0 +1,48 @@
+package NumberPK;
+
+public class NumberPk {
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public static boolean isOdd(int number) {
+        return number % 2 != 0;
+    }
+    
+    public static boolean isArmstrong(int number) {
+        int original = number, sum = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            sum += digit * digit * digit;
+            number /= 10;
+        }
+        return sum == original;
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) return false;
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static boolean isPalindrome(int number) {
+        int original = number, reversed = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+        return original == reversed;
+    }
+        public static int factorial(int number) {
+        if (number < 0) return -1;
+        int fact = 1;
+        for (int i = 2; i <= number; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+} 
+
